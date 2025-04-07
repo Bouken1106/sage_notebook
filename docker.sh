f9437a293b4b5714fc1f7fb0a7eff2bbd67a29a8
蔵満健 <kuramanken@kuramankennoiMac.local>
2025-04-07 23:20:14 +0900

    add dockerfile

--- docker.sh
+++ docker.sh
@@ -1,1 +1,8 @@
-docker run -it --rm -p 8888:8888 -e TZ=Asia/Tokyo -u $(id -u ${USER}):$(id -g ${USER}): -v ${PWD}:${PWD} -v ${PWD}/.sage:/home/sage/.sage -w ${PWD} sagemath/sagemath:latest sage-jupyter
+docker run --platform linux/amd64 --rm -it \
+  -p 8888:8888 \
+  -e TZ=Asia/Tokyo \
+  -u $(id -u ${USER}):$(id -g ${USER}) \
+  -v ${PWD}:${PWD} \
+  -v ${PWD}/.sage:/home/sage/.sage \
+  -w ${PWD} \
+  sagemath/sagemath:latest sage -n jupyter
